73ea6b392d742e2cfd833b9b4d504acf8d8e8636
Adrian clase <aluque@elpuig.xeill.net>
2026-05-05 18:45:55 +0200

    preparar el controlador de las cartas para las funciones añadir y borrar

--- backend/src/main/java/com/arcade/backend/controladores/CodejackControlador.java
+++ backend/src/main/java/com/arcade/backend/controladores/CodejackControlador.java
@@ -2,9 +2,13 @@ package com.arcade.backend.controladores;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +16,32 @@ import com.arcade.backend.entidades.CartaCodejack;
 import com.arcade.backend.repositorios.CartaCodejackRepositorio;
 
 @RestController
-@RequestMapping("/api/codejack")
-@CrossOrigin(origins = "*") 
+@RequestMapping("/api/cartas-codejack")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CodejackControlador {
 
-    @Autowired
-    private CartaCodejackRepositorio cartaRepositorio;
+    private final CartaCodejackRepositorio cartaRepositorio;
 
-    @GetMapping("/cartas")
-    public List<CartaCodejack> obtenerBaraja() {
-       
+    public CodejackControlador(CartaCodejackRepositorio cartaRepositorio) {
+        this.cartaRepositorio = cartaRepositorio;
+    }
+
+    @GetMapping
+    public List<CartaCodejack> obtenerTodas() {
         return cartaRepositorio.findAll();
+    }
+
+    @PostMapping("/add")
+    public CartaCodejack crearCarta(@RequestBody CartaCodejack nuevaCarta) {
+        return cartaRepositorio.save(nuevaCarta);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarCarta(@PathVariable Long id) {
+        if (cartaRepositorio.existsById(id)) {
+            cartaRepositorio.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
