b4d38203eb4e079210358ee93dc7c4623627b184
AdriProGamer2011 <aluque@elpuig.xeill.net>
2026-05-09 21:06:28 +0200

    base de datos preparada junto a la logica base del juego, por el momento solo esta la funcion de preparar los retos junto a la funcion de actualizar bytes, falta la logica del juego con la api nativa como tal

--- frontend/src/games/LogicLink.vue
+++ frontend/src/games/LogicLink.vue
@@ -1,13 +1,70 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
-import { useAuthStore } from '../stores/auth'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
-import { registrarPartida } from '@/services/statsService';
+import { useAuthStore } from '../stores/auth'
+import { registrarPartida } from '@/services/statsService'
 
+const router = useRouter()
+const auth = useAuthStore()
 const API_URL = import.meta.env.VITE_API_BASE_URL
 
-const auth = useAuthStore()
-const router = useRouter()
+const retos = ref<any[]>([])
+const retoActualId = ref(0)
+const opciones = ref<string[]>([])
+const estado = ref('jugando') // jugando, correcto, error, terminado
+const piezaSoltada = ref<string | null>(null)
+
+const cerrarSesion = () => {
+  auth.logout()
+  router.push('/login')
+}
+
+const cargarRetos = async () => {
+  try {
+    const res = await fetch(`${API_URL}/logiclink`)
+    if (res.ok) {
+      let datos = await res.json()
+
+      retos.value = datos.sort(() => Math.random() - 0.5)
+      prepararReto()
+    }
+  } catch (error) {
+    console.error("Error cargando los enlaces lógicos:", error)
+  }
+}
+
+const prepararReto = () => {
+  if (retos.value.length === 0) return
+  
+  estado.value = 'jugando'
+  piezaSoltada.value = null
+  
+  const retoActual = retos.value[retoActualId.value]
+  const arrayOpciones = [
+    retoActual.respuestaCorrecta, 
+    retoActual.opcionFalsa1, 
+    retoActual.opcionFalsa2
+  ]
+  
+  opciones.value = arrayOpciones.sort(() => Math.random() - 0.5)
+}
+
+const actualizarBytes = async (cantidad: number) => {
+  if (!auth.jugador) return;
+  try {
+    const res = await fetch(`${API_URL}/usuarios/${auth.jugador}/bytes?cantidad=${cantidad}`, {
+      method: 'PUT'
+    });
+    if (res.ok) {
+      const usuario = await res.json();
+      auth.bytes = usuario.saldoBytes;
+      auth.guardarEnLocal();
+    }
+  } catch (error) {
+    console.error("Error al actualizar bytes:", error);
+  }
+}
+
 
 
 </script>
